574d5f682047206f870388ae02df022a91d35f22
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2017-07-02 16:22:07 +0200

    Added Test for Put Options

--- src/FinancialModule.jl
+++ src/FinancialModule.jl
@@ -46,6 +46,7 @@ end
 export blsgamma;
 function blsgamma{A <: Number,B <: Number,C <: Number,D <: Number,E <: Number,F <: Number}(S0::A,K::B,r::C,T::D,sigma::E,d::F=0.0,flag::Bool=true)
   #Black & Scholes Gamma for European Options
+  #For coherence i left the last boolean input.
   blscheck(S0,K,r,T,sigma,d);
   d1=(log(S0/K)+(r-d+sigma*sigma*0.5)*T)/(sigma*sqrt(T));
   Out=exp(-d*T)*normpdf(d1)/(S0*sigma*sqrt(T));
@@ -55,6 +56,7 @@ end
 export blsvega;
 function blsvega{A <: Number,B <: Number,C <: Number,D <: Number,E <: Number,F <: Number}(S0::A,K::B,r::C,T::D,sigma::E,d::F=0.0,flag::Bool=true)
   #Black & Scholes Vega for European Options
+  #For coherence i left the last boolean input.
   blscheck(S0,K,r,T,sigma,d);
   d1=(log(S0/K)+(r-d+sigma*sigma*0.5)*T)/(sigma*sqrt(T));
   Out=S0*exp(-d*T)*normpdf(d1)*sqrt(T);
@@ -77,28 +79,26 @@ end
 export blstheta;
 function blstheta{A <: Number,B <: Number,C <: Number,D <: Number,E <: Number,F <: Number}(S0::A,K::B,r::C,T::D,sigma::E,d::F=0.0,flag::Bool=true)
   #Black & Scholes Theta for European Options
-  blscheck(S0,K,r,T,sigma,d);
+    blscheck(S0,K,r,T,sigma,d);
 	sqrtT       = sqrt(T);
 	sigma_sqrtT = sigma .* sqrtT;
 
 	d1 = 1 ./ sigma_sqrtT .* (log(S0 ./ K) + (r - d + sigma.^2 / 2) .* T);
-	d2 = d1 - sigma_sqrtT;
-
-	phi1 = normcdf(d1);
-	phi2 = normcdf(d2);
-
+	
 	disc  = exp(-d .* T);
 	shift = -disc .* S0 .* normpdf(d1) .* sigma / 2 ./ sqrtT;
 	t1    = r .* K   .* exp(-r .* T);
 	t2    = d .* S0 .* disc;
 	Out=0.0;
 	if (flag)
-		Out=shift - t1 .*      phi2  + t2 .*  phi1     ;
+		Out=shift - t1 .*      normcdf(d1 - sigma_sqrtT)  + t2 .*  normcdf(d1)     ;
 	else
-		Out=shift + t1 .* (1 - phi2) + t2 .* (phi1 - 1);
+		Out=shift + t1 .* (1 - normcdf(d1 - sigma_sqrtT)) + t2 .* (normcdf(d1) - 1);
 	end
 	return Out;
 end
+
+
 export blscheck;
 function blscheck{A <: Number,B <: Number,C <: Number,D <: Number,E <: Number,F <: Number}(S0::A,K::B,r::C,T::D,sigma::E,d::F=0.0)
 if isa(S0,Complex)
@@ -131,13 +131,18 @@ end
 
 export blsimpv
 using Optim
-function blsimpv{A <: Real,B <: Real,C <: Real,D <: Real,E <: Real,F <: Real}(S0::A,K::B,r::C,T::D,Price::E,d::F,flag::Bool=true)
+function blsimpv{A <: Real,B <: Real,C <: Real,D <: Real,E <: Real,F <: Real}(S0::A,K::B,r::C,T::D,Price::E,d::F=0.0,flag::Bool=true)
 if (Price< E(0))
 	error("Option Price Cannot Be Negative")
 end
 blscheck(S0,K,r,T,0.1,d);
 f(x)=(blsprice(S0,K,r,T,x,d,flag)-Price).^2.0;
-ResultsOptimization=optimize(f,0.001,1.2,Optim.Brent(),abs_tol=1e-16,rel_tol=1e-16);
+ResultsOptimization=0;
+try
+	ResultsOptimization=optimize(f,0.001,1.2,Optim.Brent(),abs_tol=1e-16,rel_tol=1e-16);
+catch 
+	error("The One-Dimensional Solver was not able to invert the BS Formula"+catch_backtrace())
+end
 Sigma=ResultsOptimization.minimizer;
 return Sigma;
 end
--- test/test1.jl
+++ test/test1.jl
@@ -1,40 +1,73 @@
 using Base.Test
 using FinancialModule
 
+#Test Parameters
 testToll=1e-14;
 spot=10;K=10;r=0.02;T=2.0;sigma=0.2;d=0.01;
-Price=blsprice(spot,K,r,T,sigma,d);
-Delta=blsdelta(spot,K,r,T,sigma,d);
+
+#EuropeanCall Option
+PriceCall=blsprice(spot,K,r,T,sigma,d);
+DeltaCall=blsdelta(spot,K,r,T,sigma,d);
+ThetaCall=blstheta(spot,K,r,T,sigma,d);
+RhoCall=blsrho(spot,K,r,T,sigma,d);
+SigmaCall=blsimpv(spot, K, r, T, PriceCall, d);
+
+#EuropeanPut Option
+PricePut=blsprice(spot,K,r,T,sigma,d,false);
+DeltaPut=blsdelta(spot,K,r,T,sigma,d,false);
+ThetaPut=blstheta(spot,K,r,T,sigma,d,false);
+RhoPut=blsrho(spot,K,r,T,sigma,d,false);
+SigmaPut=blsimpv(spot, K, r, T, PricePut, d,false);
+
+#Equals for both Options
 Gamma=blsgamma(spot,K,r,T,sigma,d);
-Theta=blstheta(spot,K,r,T,sigma,d);
-Rho=blsrho(spot,K,r,T,sigma,d);
 Vega=blsvega(spot,K,r,T,sigma,d);
-Sigma=blsimpv(spot, K, r, T, Price, d);
 
-#Standard Test
-@test(abs(Price-1.191201316999582)<testToll)
-@test(abs(Delta-0.572434050810368)<testToll)
+#Standard Test European Call Option
+@test(abs(PriceCall-1.191201316999582)<testToll)
+@test(abs(DeltaCall-0.572434050810368)<testToll)
+@test(abs(ThetaCall+0.303776337550247)<testToll)
+@test(abs(RhoCall-9.066278382208203)<testToll)
+@test(abs(SigmaCall-0.2)<testToll)
+
+
+#Standard Test European Put Option
+@test(abs(PricePut-0.997108975455260)<testToll)
+@test(abs(DeltaPut+0.407764622496387)<testToll)
+@test(abs(ThetaPut+0.209638317050458)<testToll)
+@test(abs(RhoPut+10.149510400838260)<testToll)
+@test(abs(SigmaPut-0.2)<testToll)
+
+#Standard Test for Common Sensitivities
 @test(abs(Gamma-0.135178479404601)<testToll)
-@test(abs(Theta+0.303776337550247)<testToll)
-@test(abs(Rho-9.066278382208203)<testToll)
 @test(abs(Vega-5.407139176184034)<testToll)
-@test(abs(Sigma-0.2)<testToll)
 
-#Complex Test with Complex Step Approximation
+## Complex Test with Complex Step Approximation for European Call
+#Test parameters
 DerToll=1e-13;
 di=1e-15;
 df(f,x)=f(x+1im*di)/di;
-
+#Function definition
 F(spot)=blsprice(spot,K,r,T,sigma,d);
 G(r)=blsprice(spot,K,r,T,sigma,d);
 H(T)=blsprice(spot,K,r,T,sigma,d);
 L(sigma)=blsprice(spot,K,r,T,sigma,d);
-
-
-@test(abs(df(F,spot).im-Delta)<DerToll)
-@test(abs(df(G,r).im-Rho)<DerToll)
-@test(abs(-df(H,T).im-Theta)<DerToll)
+#TEST
 @test(abs(df(L,sigma).im-Vega)<DerToll)
+#
+@test(abs(df(F,spot).im-DeltaCall)<DerToll)
+@test(abs(df(G,r).im-RhoCall)<DerToll)
+@test(abs(-df(H,T).im-ThetaCall)<DerToll)
+
+## Complex Test with Complex Step Approximation for European Put
+#Function definition
+F1(spot)=blsprice(spot,K,r,T,sigma,d,false);
+G1(r)=blsprice(spot,K,r,T,sigma,d,false);
+H1(T)=blsprice(spot,K,r,T,sigma,d,false);
+#TEST
+@test(abs(df(F1,spot).im-DeltaPut)<DerToll)
+@test(abs(df(G1,r).im-RhoPut)<DerToll)
+@test(abs(-df(H1,T).im-ThetaPut)<DerToll)
 
 #TEST OF INPUT VALIDATION
 #Negative Spot Price
@@ -44,7 +77,7 @@ L(sigma)=blsprice(spot,K,r,T,sigma,d);
 @test_throws(ErrorException, blstheta(-spot,K,r,T,sigma,d))
 @test_throws(ErrorException, blsrho(-spot,K,r,T,sigma,d))
 @test_throws(ErrorException, blsvega(-spot,K,r,T,sigma,d))
-@test_throws(ErrorException, blsimpv(-spot,K,r,T,Price,d))
+@test_throws(ErrorException, blsimpv(-spot,K,r,T,PriceCall,d))
 
 #Negative Strike Price
 @test_throws(ErrorException, blsprice(spot,-K,r,T,sigma,d))
@@ -53,7 +86,7 @@ L(sigma)=blsprice(spot,K,r,T,sigma,d);
 @test_throws(ErrorException, blstheta(spot,-K,r,T,sigma,d))
 @test_throws(ErrorException, blsrho(spot,-K,r,T,sigma,d))
 @test_throws(ErrorException, blsvega(spot,-K,r,T,sigma,d))
-@test_throws(ErrorException, blsimpv(spot,-K,r,T,Price,d))
+@test_throws(ErrorException, blsimpv(spot,-K,r,T,PriceCall,d))
 
 #Negative Time to Maturity
 @test_throws(ErrorException, blsprice(spot,K,r,-T,sigma,d))
@@ -62,7 +95,7 @@ L(sigma)=blsprice(spot,K,r,T,sigma,d);
 @test_throws(ErrorException, blstheta(spot,K,r,-T,sigma,d))
 @test_throws(ErrorException, blsrho(spot,K,r,-T,sigma,d))
 @test_throws(ErrorException, blsvega(spot,K,r,-T,sigma,d))
-@test_throws(ErrorException, blsimpv(spot,K,r,-T,Price,d))
+@test_throws(ErrorException, blsimpv(spot,K,r,-T,PriceCall,d))
 
 #Negative Volatility
 @test_throws(ErrorException, blsprice(spot,K,r,T,-sigma,d))
@@ -73,6 +106,6 @@ L(sigma)=blsprice(spot,K,r,T,sigma,d);
 @test_throws(ErrorException, blsvega(spot,K,r,T,-sigma,d))
 
 #Negative Option Price
-@test_throws(ErrorException, blsimpv(spot,K,r,T,-Price,d))
+@test_throws(ErrorException, blsimpv(spot,K,r,T,-PriceCall,d))
 
 println("All Test Passed")
