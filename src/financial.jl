825161cc34019713d8bfe77b5086465b4d96670e
rcalxrc08 <nicola.scaramuzzino@yandex.com>
2017-08-06 14:51:51 +0200

    Passing to σ

--- src/financial.jl
+++ src/financial.jl
@@ -10,14 +10,14 @@ end
 """
 Black & Scholes Price for European Options
 
-    Price=blsprice(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Price=blsprice(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -29,10 +29,10 @@ julia> blsprice(10.0,10.0,0.01,2.0,0.2,0.01)
 1.1023600107733191
 ```
 """
-function blsprice{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
-  blscheck(S0,K,r,T,sigma,d);
-  d1=(log(S0/K)+(r-d+sigma*sigma*0.5)*T)/(sigma*sqrt(T));
-  d2=d1-sigma*sqrt(T);
+function blsprice{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
+  blscheck(S0,K,r,T,σ,d);
+  d1=(log(S0/K)+(r-d+σ*σ*0.5)*T)/(σ*sqrt(T));
+  d2=d1-σ*sqrt(T);
   Out=0.0;
   if FlagIsCall
 	Out=S0*exp(-d*T)*normcdf(d1)-K*exp(-r*T)*normcdf(d2);
@@ -46,14 +46,14 @@ end
 """
 Black Price for European Options
 
-    Price=blkprice(F0,K,r,T,sigma,FlagIsCall=true)
-	
+    Price=blkprice(F0,K,r,T,σ,FlagIsCall=true)
+
 Where:\n
 	F0         = Value of the Forward.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility.
 	FlagIsCall = true for Call Options, false for Put Options.
 
 	Price      = price of the European Option.
@@ -64,9 +64,9 @@ julia> blkprice(10.0,10.0,0.01,2.0,0.2)
 1.1023600107733191
 ```
 """
-function blkprice{num1 ,num2 ,num3 ,num4 ,num5 <: Number}(F0::num1,K::num2,r::num3,T::num4,sigma::num5,FlagIsCall::Bool=true)
-  blscheck(F0,K,r,T,sigma,r);
-  Out=blsprice(F0,K,r,T,sigma,r,FlagIsCall);
+function blkprice{num1 ,num2 ,num3 ,num4 ,num5 <: Number}(F0::num1,K::num2,r::num3,T::num4,σ::num5,FlagIsCall::Bool=true)
+  blscheck(F0,K,r,T,σ);
+  Out=blsprice(F0,K,r,T,σ,r,FlagIsCall);
 return Out;
 end
 
@@ -74,14 +74,14 @@ end
 """
 Black & Scholes Delta for European Options
 
-    Delta=blsdelta(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Delta=blsdelta(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -93,9 +93,9 @@ julia> blsdelta(10.0,10.0,0.01,2.0,0.2,0.01)
 0.5452173371920436
 ```
 """
-function blsdelta{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
-  blscheck(S0,K,r,T,sigma,d);
-  d1=(log(S0/K)+(r-d+sigma*sigma*0.5)*T)/(sigma*sqrt(T));
+function blsdelta{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
+  blscheck(S0,K,r,T,σ,d);
+  d1=(log(S0/K)+(r-d+σ*σ*0.5)*T)/(σ*sqrt(T));
   Out=0.0;
   if FlagIsCall
 	Out=exp(-d*T)*normcdf(d1);
@@ -109,14 +109,14 @@ end
 """
 Black & Scholes Gamma for European Options
 
-    Gamma=blsgamma(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Gamma=blsgamma(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -128,11 +128,11 @@ julia> blsgamma(10.0,10.0,0.01,2.0,0.2,0.01)
 0.13687881535712826
 ```
 """
-function blsgamma{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
+function blsgamma{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
   #For coherence i left the last boolean input.
-  blscheck(S0,K,r,T,sigma,d);
-  d1=(log(S0/K)+(r-d+sigma*sigma*0.5)*T)/(sigma*sqrt(T));
-  Out=exp(-d*T)*normpdf(d1)/(S0*sigma*sqrt(T));
+  blscheck(S0,K,r,T,σ,d);
+  d1=(log(S0/K)+(r-d+σ*σ*0.5)*T)/(σ*sqrt(T));
+  Out=exp(-d*T)*normpdf(d1)/(S0*σ*sqrt(T));
 return Out;
 end
 
@@ -140,14 +140,14 @@ end
 """
 Black & Scholes Vega for European Options
 
-    Vega=blsvega(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Vega=blsvega(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -159,10 +159,10 @@ julia> blsvega(10.0,10.0,0.01,2.0,0.2,0.01)
 5.475152614285131
 ```
 """
-function blsvega{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
+function blsvega{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
   #For coherence i left the last boolean input.
-  blscheck(S0,K,r,T,sigma,d);
-  d1=(log(S0/K)+(r-d+sigma*sigma*0.5)*T)/(sigma*sqrt(T));
+  blscheck(S0,K,r,T,σ,d);
+  d1=(log(S0/K)+(r-d+σ*σ*0.5)*T)/(σ*sqrt(T));
   Out=S0*exp(-d*T)*normpdf(d1)*sqrt(T);
 return Out;
 end
@@ -171,14 +171,14 @@ end
 """
 Black & Scholes Rho for European Options
 
-    Rho=blsrho(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Rho=blsrho(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -190,9 +190,9 @@ julia> blsrho(10.0,10.0,0.01,2.0,0.2,0.01)
 8.699626722294234
 ```
 """
-function blsrho{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
-  blscheck(S0,K,r,T,sigma,d);
-  d2=(log(S0/K)+(r-d-sigma*sigma*0.5)*T)/(sigma*sqrt(T));
+function blsrho{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
+  blscheck(S0,K,r,T,σ,d);
+  d2=(log(S0/K)+(r-d-σ*σ*0.5)*T)/(σ*sqrt(T));
   if FlagIsCall
 	Out=K*exp(-r*T)*normcdf(d2)*T;
   else
@@ -205,14 +205,14 @@ end
 """
 Black & Scholes Theta for European Options
 
-    Theta=blstheta(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Theta=blstheta(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -224,21 +224,21 @@ julia> blstheta(10.0,10.0,0.01,2.0,0.2,0.01)
 -0.26273403060652334
 ```
 """
-function blstheta{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
-    blscheck(S0,K,r,T,sigma,d);
+function blstheta{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
+    blscheck(S0,K,r,T,σ,d);
 	sqrtT       = sqrt(T);
-	sigma_sqrtT = sigma .* sqrtT;
+	σ_sqrtT = σ .* sqrtT;
 
-	d1 = (log(S0 ./ K) + (r - d + sigma.^2 / 2) .* T)./ sigma_sqrtT;
+	d1 = (log(S0 ./ K) + (r - d + σ.^2 / 2) .* T)./ σ_sqrtT;
 
-	shift = -exp(-d .* T) .* S0 .* normpdf(d1) .* sigma / 2 ./ sqrtT;
+	shift = -exp(-d .* T) .* S0 .* normpdf(d1) .* σ / 2 ./ sqrtT;
 	t1    = r .* K   .* exp(-r .* T);
 	t2    = d .* S0 .* exp(-d .* T);
 	Out=0.0;
 	if FlagIsCall
-		Out=shift - t1 .*      normcdf(d1 - sigma_sqrtT)  + t2 .*  normcdf(d1)     ;
+		Out=shift - t1 .*      normcdf(d1 - σ_sqrtT)  + t2 .*  normcdf(d1)     ;
 	else
-		Out=shift + t1 .* (1 - normcdf(d1 - sigma_sqrtT)) + t2 .* (normcdf(d1) - 1);
+		Out=shift + t1 .* (1 - normcdf(d1 - σ_sqrtT)) + t2 .* (normcdf(d1) - 1);
 	end
 	return Out;
 end
@@ -247,14 +247,14 @@ end
 """
 Black & Scholes Lambda for European Options
 
-    Lambda=blslambda(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Lambda=blslambda(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -266,17 +266,17 @@ julia> blslambda(10.0,10.0,0.01,2.0,0.2,0.01)
 4.945909973725978
 ```
 """
-function blslambda{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
-  blscheck(S0,K,r,T,sigma,d);
-  Price=blsprice(S0,K,r,T,sigma,d,FlagIsCall);
-  tmpOut=blsdelta(S0,K,r,T,sigma,d,FlagIsCall);
+function blslambda{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
+  blscheck(S0,K,r,T,σ,d);
+  Price=blsprice(S0,K,r,T,σ,d,FlagIsCall);
+  tmpOut=blsdelta(S0,K,r,T,σ,d,FlagIsCall);
   Out=tmpOut*S0/Price;
 return Out;
 end
 
 
 "Check input for Black Scholes Formula"
-function blscheck{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0)
+function blscheck{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0)
 if isa(S0,Complex)
 	if (S0.re< S0.re*0)
 		error("Spot Price Cannot Be Negative")
@@ -289,8 +289,8 @@ elseif isa(T,Complex)
 	if (T.re< T.re*0)
 		error("Time to Maturity Cannot Be Negative")
 	end
-elseif isa(sigma,Complex)
-	if (sigma.re< sigma.re*0)
+elseif isa(σ,Complex)
+	if (σ.re< σ.re*0)
 		error("Volatility Cannot Be Negative")
 	end
 elseif (S0< num1(0))
@@ -299,7 +299,7 @@ elseif (K< num2(0))
 	error("Strike Price Cannot Be Negative")
 elseif (T< num4(0))
 	error("Time to Maturity Cannot Be Negative")
-elseif (sigma< num5(0))
+elseif (σ< num5(0))
 	error("Volatility Cannot Be Negative")
 end
 return;
@@ -313,7 +313,7 @@ function brentMethod(f::Function, x0::Number, x1::Number,xtol::AbstractFloat=1e-
 	if ytol<0.0
       error("y tollerance cannot be negative")
     end
-    EPS = eps(Float64)
+    EPS = eps(Number)
 	maxiter=80;
     y0 = f(x0)
     y1 = f(x1)
@@ -384,8 +384,8 @@ export blsimpv
 """
 Black & Scholes Implied Volatility for European Options
 
-    Volatility=blsimpv(S0,K,r,T,Price,d=0.0,FlagIsCall=true,xtol=1e-14,ytol=1e-15) 
-	
+    Volatility=blsimpv(S0,K,r,T,Price,d=0.0,FlagIsCall=true,xtol=1e-14,ytol=1e-15)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
@@ -415,16 +415,16 @@ try
 catch e
 	error("The Inversion of Black Scholes Price Failed with the following error: $e")
 end
-Sigma=ResultsOptimization;
-return Sigma;
+σ=ResultsOptimization;
+return σ;
 end
 
 export blkimpv
 """
 Black Implied Volatility for European Options
 
-    Volatility=blkimpv(S0,K,r,T,Price,d=0.0,FlagIsCall=true,xtol=1e-14,ytol=1e-15) 
-	
+    σ=blkimpv(S0,K,r,T,Price,d=0.0,FlagIsCall=true,xtol=1e-14,ytol=1e-15)
+
 Where:\n
 	S0         = Value of the Forward.
 	K          = Strike Price of the Option.
@@ -433,7 +433,7 @@ Where:\n
 	Price      = Price of the Option.
 	FlagIsCall = true for Call Options, false for Put Options.
 
-	Volatility = implied volatility of the European Option.
+	σ = implied volatility of the European Option.
 
 # Example
 ```julia-repl
@@ -442,8 +442,8 @@ julia> blkimpv(10.0,10.0,0.01,2.0,2.0)
 ```
 """
 function blkimpv{num1 ,num2 ,num3 ,num4 ,num5 <: Real}(S0::num1,K::num2,r::num3,T::num4,Price::num5,FlagIsCall::Bool=true,xtol::Real=1e-14,ytol::Real=1e-15)
-Sigma=blsimpv(S0,K,r,T,Price,r,FlagIsCall,xtol,ytol)
-return Sigma;
+σ=blsimpv(S0,K,r,T,Price,r,FlagIsCall,xtol,ytol)
+return σ;
 end
 
 ##	ADDITIONAL Function
@@ -451,14 +451,14 @@ end
 """
 Black & Scholes Psi for European Options
 
-    Psi=blspsi(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Psi=blspsi(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -470,9 +470,9 @@ julia> blspsi(10.0,10.0,0.01,2.0,0.2,0.01)
 -10.904346743840872
 ```
 """
-function blspsi{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
-  blscheck(S0,K,r,T,sigma,d);
-  d1=(log(S0/K)+(r-d+sigma*sigma*0.5)*T)/(sigma*sqrt(T));
+function blspsi{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
+  blscheck(S0,K,r,T,σ,d);
+  d1=(log(S0/K)+(r-d+σ*σ*0.5)*T)/(σ*sqrt(T));
   if FlagIsCall
 	Out=-S0*exp(-d*T)*normcdf(d1)*T;
   else
@@ -485,14 +485,14 @@ end
 """
 Black & Scholes Vanna for European Options
 
-    Vanna=blsvanna(S0,K,r,T,sigma,d=0.0,FlagIsCall=true)
-	
+    Vanna=blsvanna(S0,K,r,T,σ,d=0.0,FlagIsCall=true)
+
 Where:\n
 	S0         = Value of the Underlying.
 	K          = Strike Price of the Option.
 	r          = Zero Rate.
 	T          = Time to Maturity of the Option.
-	sigma      = Implied Volatility.
+	σ          = Implied Volatility
 	d          = Implied Dividend of the Underlying.
 	FlagIsCall = true for Call Options, false for Put Options.
 
@@ -504,10 +504,10 @@ julia> blsvanna(10.0,10.0,0.01,2.0,0.2,0.01)
 0.2737576307142566
 ```
 """
-function blsvanna{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,sigma::num5,d::num6=0.0,FlagIsCall::Bool=true)
-  blscheck(S0,K,r,T,sigma,d);
-  d1=(log(S0/K)+(r-d+sigma*sigma*0.5)*T)/(sigma*sqrt(T));
-  d2=d1-sigma*sqrt(T);
-  Out=-exp(-d*T)*normpdf(d1)*d2/sigma;
+function blsvanna{num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}(S0::num1,K::num2,r::num3,T::num4,σ::num5,d::num6=0.0,FlagIsCall::Bool=true)
+  blscheck(S0,K,r,T,σ,d);
+  d1=(log(S0/K)+(r-d+σ*σ*0.5)*T)/(σ*sqrt(T));
+  d2=d1-σ*sqrt(T);
+  Out=-exp(-d*T)*normpdf(d1)*d2/σ;
 return Out;
 end
