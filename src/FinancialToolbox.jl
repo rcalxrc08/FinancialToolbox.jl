52f867656d675e62317069bd25df06abfdbe8827
Unknown <nicola.scaramuzzino@yandex.com>
2019-04-24 22:37:42 +0200

    fwd diff

--- src/FinancialToolbox.jl
+++ src/FinancialToolbox.jl
@@ -6,6 +6,7 @@ module FinancialToolbox
 	include("dates.jl");
 	function __init__()
 		@require DualNumbers = "fa6b7ba4-c1ee-5f82-b5fc-ecf0adba8f74" include("financial_dual.jl")
+		@require ForwardDiff = "f6369f11-7733-5829-9624-2563aa707210" include("financial_fwd_diff.jl")
 	end
 	include("financial.jl")
 	export
---
+++ src/financial_fwd_diff.jl
@@ -0,0 +1,24 @@
+using .ForwardDiff
+function blsimpv(S0::num1,K::num2,r::num3,T::num4,Price::num5,d::num6=0.0,FlagIsCall::Bool=true,xtol::Real=1e-14,ytol::Real=1e-15) where {num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}
+if (Price< num5(0))
+	throw(ErrorException("Option Price Cannot Be Negative"));
+end
+FinancialToolbox.blscheck(S0,K,r,T,0.1,d);
+value__(x)=x.value;
+value__(x::Real)=x;
+f(x)=(blsprice(value__(S0),value__(K),value__(r),value__(T),x,value__(d),FlagIsCall)-value__(Price));
+σ=FinancialToolbox.brentMethod(f,0.001,1.2,xtol,ytol);
+der_=-(blsprice(S0,K,r,T,σ,d,FlagIsCall)/blsvega(value__(S0),value__(K),value__(r),value__(T),σ,value__(d),FlagIsCall)).partials
+out=Dual(σ,der_);
+
+return out;
+
+end
+
+function blkimpv(S0::num1,K::num2,r::num3,T::num4,Price::num5,FlagIsCall::Bool=true,xtol::Real=1e-14,ytol::Real=1e-15) where {num1 ,num2 ,num3 ,num4 ,num5 ,num6 <: Number}
+
+blscheck(S0,K,r,T,0.1,r);
+
+return blsimpv(S0,K,r,T,Price,r,FlagIsCall);
+
+end
