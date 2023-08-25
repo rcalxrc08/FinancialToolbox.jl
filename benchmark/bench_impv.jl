99a477eb08a1bd8c67af6ed77dbc688b09cb3e0b
Nicola <nicola.scaramuzzino@yandex.com>
2023-08-25 12:30:31 +0200

    add support for impv and related benchmark

---
+++ benchmark/bench_impv.jl
@@ -0,0 +1,19 @@
+## Bench pricer function
+using FinancialToolbox, BenchmarkTools, ForwardDiff, ReverseDiff, Zygote
+suite = BenchmarkGroup()
+S0 = 100.0;
+K = 100.0;
+r = 0.01;
+T = 1.0;
+sigma = 0.2;
+d = 0.01;
+price=blsprice(S0, K, r, T, sigma, d);
+const inputs = [S0, K, r, T, price, d];
+output = similar(inputs);
+suite["implied volatility forward"] = @benchmarkable @views ForwardDiff.gradient!(output, x -> blsimpv(x[1], x[2], x[3], x[4], x[5], x[6]), $inputs);
+suite["implied volatility ReverseDiff"] = @benchmarkable @views ReverseDiff.gradient(x -> blsimpv(x[1], x[2], x[3], x[4], x[5], x[6]), $inputs);
+cfg = ReverseDiff.GradientConfig(similar(inputs))
+const f_tape = ReverseDiff.compile(ReverseDiff.GradientTape(x -> blsimpv(x[1], x[2], x[3], x[4], x[5], x[6]), inputs, cfg))
+suite["implied volatility ReverseDiff compiled"] = @benchmarkable ReverseDiff.gradient!($output, $f_tape, $inputs);
+@btime Zygote.gradient(blsimpv, $S0, $K, $r, $T, $price, $d);
+suite["implied volatility Zygote"] = @benchmarkable Zygote.gradient(blsimpv, $S0, $K, $r, $T, $price, $d);
--- src/FinancialToolbox.jl
+++ src/FinancialToolbox.jl
@@ -6,6 +6,8 @@ using Dates, Requires, ChainRulesCore
 include("dates.jl")
 function __init__()
     @require DualNumbers = "fa6b7ba4-c1ee-5f82-b5fc-ecf0adba8f74" include("financial_dual.jl")
+    @require ForwardDiff = "f6369f11-7733-5829-9624-2563aa707210" include("financial_forward_diff.jl")
+    @require ReverseDiff = "37e2e3b7-166d-5795-8a7a-e32c996b4267" include("financial_reverse_diff.jl")
     @require HyperDualNumbers = "50ceba7f-c3ee-5a84-a6e8-3ad40456ec97" include("financial_hyper.jl")
     @require TaylorSeries = "6aa5eb33-94cf-58f4-a9d0-e4b2c4fc25ea" include("financial_taylor.jl")
 end
