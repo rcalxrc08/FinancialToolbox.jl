d5d4bc0e7707ccb8e474539a7ca062683f0f29e2
Nicola <nicola.scaramuzzino@yandex.com>
2023-08-30 19:35:58 +0200

    recover coverage

--- src/financial_taylor.jl
+++ src/financial_taylor.jl
@@ -1,17 +1,17 @@
 using .TaylorSeries
 
-function integrate_correct(a::Taylor1{T}, x::S) where {T <: Number, S <: Number}
-    order = get_order(a)
-    @views aa = a[0] / 1 + zero(x)
-    R = typeof(aa)
-    coeffs = Array{typeof(aa)}(undef, order + 2)
-    # fill!(coeffs, zero(aa))
-    @inbounds @views coeffs[1] = convert(R, x)
-    @inbounds for i = 1:order+1
-        @views coeffs[i+1] = a[i-1] / i
-    end
-    return Taylor1(coeffs)
-end
+# function integrate_correct(a::Taylor1{T}, x::S) where {T <: Number, S <: Number}
+#     order = get_order(a)
+#     @views aa = a[0] / 1 + zero(x)
+#     R = typeof(aa)
+#     coeffs = Array{typeof(aa)}(undef, order + 2)
+#     # fill!(coeffs, zero(aa))
+#     @inbounds @views coeffs[1] = convert(R, x)
+#     @inbounds for i = 1:order+1
+#         @views coeffs[i+1] = a[i-1] / i
+#     end
+#     return Taylor1(coeffs)
+# end
 
 function mul_internal(f_der::Taylor1{T}, x::Taylor1{T}, k::Int) where {T <: Number}
     k_1 = k + 1
@@ -46,34 +46,34 @@ function FinancialToolbox.normcdf(x::Taylor1)
     return mul_and_integrate(normpdf_res, x, normcdf(val))
 end
 
-function mul2!(c::HomogeneousPolynomial, a::HomogeneousPolynomial, b::HomogeneousPolynomial)
-    (iszero(b) || iszero(a)) && return nothing
+# function mul2!(c::HomogeneousPolynomial, a::HomogeneousPolynomial, b::HomogeneousPolynomial)
+#     (iszero(b) || iszero(a)) && return nothing
 
-    @inbounds num_coeffs_a = size_table[a.order+1]
-    @inbounds num_coeffs_b = size_table[b.order]
+#     @inbounds num_coeffs_a = size_table[a.order+1]
+#     @inbounds num_coeffs_b = size_table[b.order]
 
-    @inbounds posTb = pos_table[c.order+1]
+#     @inbounds posTb = pos_table[c.order+1]
 
-    @inbounds indTa = index_table[a.order+1]
-    @inbounds indTb = index_table[b.order]
+#     @inbounds indTa = index_table[a.order+1]
+#     @inbounds indTb = index_table[b.order]
 
-    @inbounds for na = 1:num_coeffs_a
-        ca = a[na]
-        # iszero(ca) && continue
-        inda = indTa[na]
+#     @inbounds for na = 1:num_coeffs_a
+#         ca = a[na]
+#         # iszero(ca) && continue
+#         inda = indTa[na]
 
-        @inbounds for nb = 1:num_coeffs_b
-            cb = b[1+nb]
-            # iszero(cb) && continue
-            indb = indTb[nb]
+#         @inbounds for nb = 1:num_coeffs_b
+#             cb = b[1+nb]
+#             # iszero(cb) && continue
+#             indb = indTb[nb]
 
-            pos = posTb[inda+indb]
-            c[pos] += ca * cb
-        end
-    end
+#             pos = posTb[inda+indb]
+#             c[pos] += ca * cb
+#         end
+#     end
 
-    return nothing
-end
+#     return nothing
+# end
 function mul_and_integrate(f_der::TaylorN{T}, x::TaylorN{T}, y::S) where {T <: Number, S <: Number}
     order = get_order(x)
     res_t = Taylor1(order)
@@ -91,31 +91,31 @@ function FinancialToolbox.normcdf(x::TaylorN)
     dx = (x - xmin) / (Nmax - 1)
     return sum(FinancialToolbox.normpdf.(x_)) * dx
 end
-function normcdf2(x::TaylorN)
-    @views val = x[0][1]
-    normpdf_res = normpdf(x)
-    grad = TaylorSeries.gradient(x)
-    # PriceCall2-integrate(derivative(PriceCall2,3),3,PriceCall2[0][1])
-    extreme_points2 = [(x - val - integrate(derivative(x, i), i)) for i in eachindex(grad)]
-    # normpdf_res_adj = [(normpdf_res[0][1] + integrate(derivative(normpdf_res, i), i)) for i in eachindex(grad)]
-    # @show grad
-    # extreme_points2 = [sum(integrate(derivative(x, j), j) for j in eachindex(grad) if i != j) for i in eachindex(grad)]
-    @show extreme_points2
-    return normcdf(val) + sum(integrate(normpdf_res * grad[i], i, extreme_points2[i]) for i in eachindex(grad)) #- tmp_el
-end
+# function normcdf2(x::TaylorN)
+#     @views val = x[0][1]
+#     normpdf_res = normpdf(x)
+#     grad = TaylorSeries.gradient(x)
+#     # PriceCall2-integrate(derivative(PriceCall2,3),3,PriceCall2[0][1])
+#     extreme_points2 = [(x - val - integrate(derivative(x, i), i)) for i in eachindex(grad)]
+#     # normpdf_res_adj = [(normpdf_res[0][1] + integrate(derivative(normpdf_res, i), i)) for i in eachindex(grad)]
+#     # @show grad
+#     # extreme_points2 = [sum(integrate(derivative(x, j), j) for j in eachindex(grad) if i != j) for i in eachindex(grad)]
+#     @show extreme_points2
+#     return normcdf(val) + sum(integrate(normpdf_res * grad[i], i, extreme_points2[i]) for i in eachindex(grad)) #- tmp_el
+# end
 
-function normcdf3(x::TaylorN)
-    @views val = x[0][1]
-    normpdf_res = normpdf(x)
-    # grad = TaylorSeries.gradient(x)
-    # PriceCall2-integrate(derivative(PriceCall2,3),3,PriceCall2[0][1])
-    # extreme_points = [(x - val - integrate(derivative(x, i), i)) for i in eachindex(grad)]
-    # extreme_points = [(x - val - integrate(derivative(x, i), i)) for i in eachindex(grad)]
-    # @show extreme_points
-    # TaylorN([HomogeneousPolynomial([0.0]), HomogeneousPolynomial([0.0, 1.0, 1.0])])
-    return normcdf(x[0][1]) + integrate(normpdf_res * derivative(x, 1), 1, x - integrate(derivative(x, 1), 1, x[0][1]))
-    # return normcdf(val) + sum(integrate(normpdf_res * grad[i], i, extreme_points[i][0][1]) for i in eachindex(grad)) #- tmp_el
-end
+# function normcdf3(x::TaylorN)
+#     @views val = x[0][1]
+#     normpdf_res = normpdf(x)
+#     # grad = TaylorSeries.gradient(x)
+#     # PriceCall2-integrate(derivative(PriceCall2,3),3,PriceCall2[0][1])
+#     # extreme_points = [(x - val - integrate(derivative(x, i), i)) for i in eachindex(grad)]
+#     # extreme_points = [(x - val - integrate(derivative(x, i), i)) for i in eachindex(grad)]
+#     # @show extreme_points
+#     # TaylorN([HomogeneousPolynomial([0.0]), HomogeneousPolynomial([0.0, 1.0, 1.0])])
+#     return normcdf(x[0][1]) + integrate(normpdf_res * derivative(x, 1), 1, x - integrate(derivative(x, 1), 1, x[0][1]))
+#     # return normcdf(val) + sum(integrate(normpdf_res * grad[i], i, extreme_points[i][0][1]) for i in eachindex(grad)) #- tmp_el
+# end
 
 value__d(x::Taylor1) = @views x[0]
 value__d(x::TaylorN) = @views x[0][1]
