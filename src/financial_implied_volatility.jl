b8131df6f065b1eb83c2795cae6714c9c0be499c
Nicola <nicola.scaramuzzino@yandex.com>
2023-09-20 17:57:43 +0200

    more efficient implied volatility

--- src/financial_implied_volatility.jl
+++ src/financial_implied_volatility.jl
@@ -7,10 +7,11 @@ function brentMethod(f::Function, x0::Number, x1::Number, xtol::AbstractFloat = 
         throw(ErrorException("y tollerance cannot be negative"))
     end
     EPS = eps(x0)
-    maxiter = 80
+    maxiter = 800
     y0 = f(x0)
     y1 = f(x1)
     if (y0 * y1 > 0)
+        @warn "There is no such volatility"
         throw(DomainError("There is no such volatility"))
     end
     if abs(y0) < abs(y1)
@@ -66,11 +67,13 @@ function brentMethod(f::Function, x0::Number, x1::Number, xtol::AbstractFloat = 
             y0, y1 = y1, y0
         end
     end
+    @warn "Max iteration exceeded, possible wrong result"
     throw(ErrorException("Max iteration exceeded, possible wrong result"))
 end
 
 function blprice_and_vega(S0, K, x, sqrtT, σ, FlagIsCall)
-    ChainRulesCore.@ignore_derivatives(FinancialToolbox.blcheck(S0, K, sqrtT, σ))
+    #I avoid the checks for the inputs since we have already checked them apart of volatility.
+    #The volatility is positive by construction.
     d1 = sqrtT * (x / σ + σ / 2)
     d2 = d1 - σ * sqrtT
     iscall = ChainRulesCore.@ignore_derivatives(ifelse(FlagIsCall, 1, -1))
@@ -79,31 +82,27 @@ function blprice_and_vega(S0, K, x, sqrtT, σ, FlagIsCall)
     return Price, ν
 end
 
-function iter_blimpv(S0, K, x, sqrtT, price, FlagIsCall, cur_sigma)
-    cur_price, cur_vega = blprice_and_vega(S0, K, x, sqrtT, cur_sigma, FlagIsCall)
-    new_sigma = cur_sigma + (price - cur_price) / cur_vega
-    eps_adj = eps(typeof(new_sigma))
-    new_sigma = max(new_sigma, eps_adj)
-    return ifelse(isnan(new_sigma), eps_adj, new_sigma)
+function iter_blimpv(S0, K, x, sqrtT, price, FlagIsCall, σ, eps_adj)
+    cur_price, cur_vega = blprice_and_vega(S0, K, x, sqrtT, σ, FlagIsCall)
+    σ_new = max(σ + (price - cur_price) / cur_vega, eps_adj)
+    return ifelse(isnan(σ_new), eps_adj, σ_new)
 end
 
 function fixed_point_blimpv(S0, K, T, price, FlagIsCall, xtol, ytol)
-    if xtol <= 0.0
-        throw(ErrorException("x tollerance cannot be negative"))
-    end
     num, den = ifelse(FlagIsCall, (S0, K), (K, S0))
     res = (num - price) / den
     sqrtT = sqrt(T)
     x = log(S0 / K) / T
-    cur_sigma = sqrt(abs(log(res)))
+    σ_cur = sqrt(abs(log(res)))
+    eps_adj = eps(typeof(x + price))
     max_iter = 80
     for _ = 1:max_iter
-        new_sigma = iter_blimpv(S0, K, x, sqrtT, price, FlagIsCall, cur_sigma)
-        diff = abs(new_sigma - cur_sigma)
+        σ_new = iter_blimpv(S0, K, x, sqrtT, price, FlagIsCall, σ_cur, eps_adj)
+        diff = abs(σ_new - σ_cur)
         if diff < xtol
-            return new_sigma
+            return σ_new
         end
-        cur_sigma = new_sigma
+        σ_cur = σ_new
     end
     @warn "max number of iterations reached, switching to bracketing method."
     # f(σ)=ifelse(σ>=0.0,price_t-blsprice2(S0, K, r, T, σ, d),price_t-blsprice2(S0, K, r, T, σ, d)-S0*exp(-r*T)+K*exp(-d*T))
@@ -111,23 +110,41 @@ function fixed_point_blimpv(S0, K, T, price, FlagIsCall, xtol, ytol)
     f(x) = blprice(S0, K, T, x, FlagIsCall) - price
     zero_typed = 0 * +(S0, K, T, price)
     σ_min = zero_typed + 1 // 100000
-    σ_max = zero_typed + 102 // 10
+    σ_max = zero_typed + σ_cur * 10
     σ = brentMethod(f, σ_min, σ_max, xtol, ytol)
     return σ
 end
 
+function blimpv_check(S0::num1, K::num2, T::num4) where {num1, num2, num4}
+    lesseq(x::Complex, y::Complex) = real(x) <= real(y)
+    lesseq(x, y) = x <= y
+    if (lesseq(S0, zero(num1)))
+        error("Spot Price Cannot Be Negative")
+    elseif (lesseq(K, zero(num2)))
+        error("Strike Price Cannot Be Negative")
+    elseif (lesseq(T, zero(num4)))
+        error("Time to Maturity Cannot Be Negative")
+    end
+    return
+end
+
 function blimpv_impl(::AbstractFloat, S0, K, T, price_d, FlagIsCall, xtol, ytol)
-    ChainRulesCore.@ignore_derivatives(FinancialToolbox.blcheck(S0, K, T))
-    if ytol <= 0.0
-        throw(ErrorException("y tollerance cannot be negative"))
-    end
-    max_price = ChainRulesCore.@ignore_derivatives(ifelse(FlagIsCall, S0, K))
-    if max_price <= price_d
-        throw(ErrorException("Price is reaching maximum value"))
-    end
-    min_price = ChainRulesCore.@ignore_derivatives(eps(zero(price_d)))
-    if min_price >= price_d
-        throw(ErrorException("Price is reaching minimum value"))
+    ChainRulesCore.ignore_derivatives() do
+        FinancialToolbox.blimpv_check(S0, K, T)
+        if xtol <= 0.0
+            throw(DomainError(xtol, "x tollerance cannot be negative"))
+        end
+        if ytol <= 0.0
+            throw(DomainError(ytol, "y tollerance cannot be negative"))
+        end
+        max_price = ifelse(FlagIsCall, S0, K)
+        if max_price <= price_d
+            throw(DomainError(price_d, "Price is reaching maximum value"))
+        end
+        min_price = eps(zero(price_d))
+        if min_price >= price_d
+            throw(DomainError(price_d, "Price is reaching minimum value"))
+        end
     end
     return fixed_point_blimpv(S0, K, T, price_d, FlagIsCall, xtol, ytol)
 end
@@ -202,12 +219,16 @@ function rrule(config::RuleConfig{>:HasReverseMode}, ::typeof(blimpv), S0, K, T,
     σ = blimpv(S0, K, T, price_d, FlagIsCall, xtol, ytol)
     function update_pullback(slice)
         _, pullback_blprice = ChainRulesCore.rrule_via_ad(config, blprice_impl, S0, K, T, σ, FlagIsCall)
-        dy = @thunk(pullback_blprice(slice))
-        @views der_S0 = dy[2]
-        @views der_K = dy[3]
-        @views der_T = dy[4]
-        @views slice_mod = @thunk(-inv(dy[5]))
-        return NoTangent(), @thunk(slice_mod * der_S0), @thunk(slice_mod * der_K), @thunk(slice_mod * der_T), @thunk(-slice_mod), NoTangent(), NoTangent(), NoTangent()
+        _, der_S0, der_K, der_T, der_σ = pullback_blprice(slice)
+        slice_mod = -inv(der_σ)
+        return NoTangent(), slice_mod * der_S0, slice_mod * der_K, slice_mod * der_T, -slice_mod, NoTangent(), NoTangent(), NoTangent()
     end
     return σ, update_pullback
 end
+
+# function frule(config::RuleConfig{>:HasForwardsMode}, (Δf, dS, dK, dT, dprice, _, _, _), ::typeof(blimpv), S0, K, T, price_d, FlagIsCall::Bool, xtol::AbstractFloat, ytol::AbstractFloat)
+#     σ = blimpv(S0, K, T, price_d, FlagIsCall, xtol, ytol)
+#     vega = blvega_impl(S0, K, T, σ)
+#     _, der_fwd = ChainRulesCore.frule_via_ad(config, (Δf, dS, dK, dT, dprice), blprice_impl, S0, K, T, σ, FlagIsCall)
+#     return σ, -der_fwd / vega
+# end
